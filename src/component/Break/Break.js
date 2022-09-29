b6a1b806d5e3693065da67f6513a5571af1ed520
JsArafath <arafatrahman077777@gmail.com>
2022-09-30 02:33:40 +0600

    break-part

--- src/component/Break/Break.js
+++ src/component/Break/Break.js
@@ -1,31 +1,31 @@
 import React from 'react';
 import './Break.css';
 
-const Break = ({handelerBreakTime}) => {
+const Break = ({ handelerBreakTime }) => {
 
 
     return (
         <div className='break'>
             <p className='break-title'><strong>Add A Break</strong></p>
-            <div className='break-info'>
+            <div className='break-details'>
                 <div>
-                   <button onClick={()=>handelerBreakTime(10)}>10s</button>
+                    <button onClick={() => handelerBreakTime(10)}>10s</button>
                 </div>
 
                 <div>
-                    <button onClick={()=>handelerBreakTime(20)}>20s</button>
+                    <button onClick={() => handelerBreakTime(20)}>20s</button>
                 </div>
 
                 <div>
-                    <button onClick={()=>handelerBreakTime(30)}>30s</button>
+                    <button onClick={() => handelerBreakTime(30)}>30s</button>
                 </div>
 
                 <div>
-                    <button onClick={()=>handelerBreakTime(40)}>40s</button>
+                    <button onClick={() => handelerBreakTime(40)}>40s</button>
                 </div>
 
             </div>
-            
+
         </div>
     );
 };
